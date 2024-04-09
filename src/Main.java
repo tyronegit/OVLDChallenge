24d125adf78a29215e3c766e34fc5370d448d57e
T.Atkins <43272999+tyronegit@users.noreply.github.com>
2024-04-09 16:40:20 -0400

    Changed name of project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double convertToCentimeters(int height){
+        double centimeters = 2.54;
+        double total = height * centimeters;
+        return total;
+    }
+}
